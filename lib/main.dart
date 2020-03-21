3a02d64b41da5bf5fbb3ed5e612f356961d594f2
jaskarans2000 <36985639+jaskarans2000@users.noreply.github.com>
2020-03-21 17:53:33 +0530

    Updated the business card

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() => runApp(MaterialApp(
   home: Home(),
@@ -10,57 +11,64 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("BizCard"),
+          title: Text("BizCard")
       ),
-
       body: Container(
         alignment: Alignment.center,
         child: Stack(
-          alignment: Alignment.center,
+          alignment: Alignment.topCenter,
           children: <Widget>[
-            Container(
-              height: 200.0,
-              width: 400.0,
-              margin: const EdgeInsets.all(20.0),
-              decoration: BoxDecoration(
-                  color: Colors.pinkAccent,
-                  borderRadius: BorderRadius.circular(30.0)
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text("Jaskaran Singh",
-                    style: TextStyle(color: Colors.white,fontSize: 20.0),),
-                  Text("Developersingh(YouTube)",style: TextStyle(fontSize: 16.0),),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Icon(Icons.person_outline),
-                      Text("T.: @buldappswithme",style: TextStyle(fontSize: 16.0),),
-                    ],
-                  )
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(bottom:200.0),
-              child: Container(
-                height: 100.0,
-                width: 100.0,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(50.0),
-                    border: Border.all(
-                        color: Colors.red,
-                        width: 1.2
-                    ),
-                    image: DecorationImage(image: NetworkImage("https://picsum.photos/seed/picsum/200/300"),
-                        fit: BoxFit.cover)
-                ),
-              ),
-            ),
+            getCard(),
+            getAvatar(),
           ],
         ),
       ),
     );
   }
+
+  Container getCard() {
+    return Container(
+      height: 200,
+      width: 350,
+      margin: const EdgeInsets.only(top:50.0,left: 10.0,right: 10.0),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15.0),
+          color: Colors.pinkAccent
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(bottom:6.0),
+            child: Text("Jaskaran Singh",
+              style: TextStyle(color: Colors.white,fontSize: 20.9,fontWeight: FontWeight.bold),),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom:6.0),
+            child: Text("Youtube Developer Singh"),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Icon(Icons.person_outline),
+              Text("developersingh07@gmail.com")
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  getAvatar() {
+    return Container(
+        height: 100.0,
+        width: 100.0,
+        decoration: BoxDecoration(
+            image: DecorationImage(image: NetworkImage("https://picsum.photos/seed/picsum/200/300",),fit: BoxFit.cover),
+            borderRadius: BorderRadius.all(Radius.circular(50.0)),
+            border: Border.all(color: Colors.redAccent,style: BorderStyle.solid,width: 1.2)
+        )
+    );
+  }
+
 }
